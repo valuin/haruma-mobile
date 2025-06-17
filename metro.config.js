672979f923e635c1663957a6e982527a3ee6a1f4
Valtrizt Khalifah Warri <valvaltrizt@gmail.com>
2025-06-17 22:25:26 +0700

    chore: bump deps and compat with metro

--- metro.config.js
+++ metro.config.js
@@ -4,3 +4,10 @@ const { withNativeWind } = require("nativewind/metro");
 const config = getDefaultConfig(__dirname);
 
 module.exports = withNativeWind(config, { input: "./globals.css" });
+module.exports = {
+  resolver: {
+    unstable_conditionNames: ['browser', 'require', 'react-native'],
+  },
+};
+
+module.exports = config;
